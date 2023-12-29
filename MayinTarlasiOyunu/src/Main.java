9c73b57cdcc15f9462ca393582ea70da68daf8e6
İsmet Onur ŞAHİN <110236136+ismetonursahin@users.noreply.github.com>
2023-12-29 19:00:49 +0300

    MayinTarlasiOyunu

--- MayinTarlasiOyunu/src/Main.java
+++ MayinTarlasiOyunu/src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Display welcome message
-        System.out.println("---- MAYIN TARLASI OYUNU ----");
+        System.out.println("---- MINE SWEEPER GAME ----");
         Scanner input = new Scanner(System.in);
 
         // Prompt user for the number of rows
-        System.out.print("Satır Sayısı Giriniz : ");
+        System.out.print("Enter Number of Row: ");                 // Değerlendirme Formu-7
         while (!input.hasNextInt()) {
             // Checking that the user entered a number
-            System.out.println("Bir Sayı Girmelisin !");
+            System.out.println("You must a number!");
             input.next();
         }
         int row = input.nextInt();
@@ -18,22 +18,22 @@ public class Main {
         // Row numbers must be at least two
         while (row < 2) {
             // Checking whether the user entered a number greater than 2
-            System.out.println("2 veya daha büyük bir sayı girmelisin !");
+            System.out.println("You must enter a number 2 or greater");
             row = input.nextInt();
         }
 
         // Prompt user for the number of columns
-        System.out.print("Sütun Sayısı Giriniz : ");
+        System.out.print("Enter Number of Column: ");
         while (!input.hasNextInt()) {
             // Checking that the user entered a number
-            System.out.println("Bir Sayı Girmelisin !");
+            System.out.println("You must a number!");
             input.next();
         }
         int col = input.nextInt();
         // Ensure that the number of columns is at least 2
         while (col < 2) {
             // Checking whether the user entered a number greater than 2
-            System.out.print("2 veya daha büyük bir sayı girmelisin !");
+            System.out.print("You must enter a number 2 or greater");
             col = input.nextInt();
         }
 
@@ -41,22 +41,23 @@ public class Main {
         MineSweeper mine = new MineSweeper(row, col);   // Creating a minesweeper class && get parameters
         mine.placeMines();             // Randomly place mines on the game board
 
+
         while (!mine.gameOver) {
-            System.out.print("Satır Koordinatı : ");
+            System.out.print("Enter a row coordinate : ");
             while (!input.hasNextInt()) {
-                System.out.print("Bir Sayı Girmelisin !");
+                System.out.print("You must a number!");        // Değerlendirme Formu -9
                 input.next();
             }
             int coordX = input.nextInt();
 
             while (coordX > row || coordX < 0) {
-                System.out.print("X Koordinatı Satır Değerinden Küçük Olamaz!");
+                System.out.print("Coordinate cannot be less than Number of Rows");   //Değerlendirme - 10
                 coordX = input.nextInt();
             }
 
-            System.out.print("Sütun Koordinatı : ");
+            System.out.print("Enter a column coordinate : ");
             while (!input.hasNextInt()) {
-                System.out.println("Bir Sayı Girmelisin !");
+                System.out.println("You must a number!");
                 input.next();
             }
             int coordY = input.nextInt();
@@ -64,7 +65,7 @@ public class Main {
                 // Ensure that the column coordinate is within the valid range
             while (coordY > col || coordY < 0) {
                 // Input validation: Ensure the user enters a valid column coordinate
-                System.out.print("Y Koordinatı Sütun Değerinden Küçük Olamaz!");
+                System.out.print("Coordinate cannot be less than Number of columns");
                 coordY = input.nextInt();
             }
 
--- MayinTarlasiOyunu/src/MineSweeper.java
+++ MayinTarlasiOyunu/src/MineSweeper.java
@@ -6,14 +6,13 @@ public class MineSweeper {
     int row;
     int col;
     String[][] userBoard; // User's game board
-    String[][] matrix;    // Matrix representing the locations of mines
     String[] mineList;   // Array to store the locations of mines
+    String[] inputList;   // Array to store of inputs
     String givenCoord;    // User input coordinates
     int size;
     int givenRow;          // User input row
     int givenCol;          // User input column
     boolean gameOver = false;    // Flag to indicate whether the game is over
-
     int openedCellsCount = 0;
 
     MineSweeper(int row, int col) {
@@ -21,18 +20,18 @@ public class MineSweeper {
         this.col = col;
         this.userBoard = new String[row][col];
         this.size = row * col;
-        this.matrix = new String[row][col];
+        this.inputList = new String[size];
     }
 
     // Method to randomly place mines
     public void placeMines() {
         int randomRow, randomCol, count = 0;
-        mineList = new String[size / 4];
+        mineList = new String[size / 4];                   // Değerlendirme Formu - 8
         while (count != (size / 4)) {
             randomRow = (int) (Math.random() * row);
             randomCol = (int) (Math.random() * col);
             String randomMine = randomRow + "," + randomCol;
-            boolean isMatch = false;              // Check if the generated mine is already added
+            boolean isMatch = false; // Check if the generated mine is already added
             for (int i = 0; i < mineList.length; i++) {
                 if (mineList[i] != null && mineList[i].equals(randomMine)) {
                     isMatch = true;
@@ -45,20 +44,21 @@ public class MineSweeper {
             mineList[count] = randomRow + "," + randomCol;
             count++;
         }
-        System.out.println(Arrays.toString(mineList) + " MAYINLAR BU BÖLGELERDE");
+       // System.out.println(Arrays.toString(mineList) + " MINES ARE PLACED IN THESE AREAS");
     }
 
     // Method to draw the user's board
     public void draw() {
         if (checkCoordInMineList(givenCoord)) {
-            System.out.println("GAME OVER !");
-            gameOver = true;
+            System.out.println("GAME OVER !");  // Değerlendirme - 15
+            gameOver = true;                    // Değerlendirme  - 13
             return;
         }
+
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if ((i + "," + j).equals(givenCoord)) {
-                    userBoard[i][j] = checkValueofCoord();
+                    userBoard[i][j] = checkValueofCoord();   //Değerlendirme -12
                 } else if (userBoard[i][j] == null) {
                     userBoard[i][j] = "-1";
                 } else if (userBoard[i][j].equals("-")) {
@@ -70,6 +70,19 @@ public class MineSweeper {
             }
             System.out.println();
         }
+    }
+
+    // Method to check if coordinates are in the list of mines
+    public boolean checkCoordInInputList() {
+        boolean isMatch = false;
+       // System.out.println(givenCoord);
+        for (int i = 0; i < openedCellsCount; i++) {
+            if (inputList[i].equals(givenCoord)) {
+                return !isMatch;
+            }
+        }
+        inputList[openedCellsCount] = givenCoord;
+        return isMatch;
     }
 
     // Method to check if coordinates are in the list of mines
@@ -105,27 +118,29 @@ public class MineSweeper {
 
     // Checking if the game is won or not
     public boolean checkGameWon() {
-        //If all the cells are opened and the game is not over, the game is won.
-        return openedCellsCount == (row * col - mineList.length) && !gameOver;
+        // If all the cells are opened and the game is not over, the game is won.
+        return openedCellsCount + 1 == (row * col - mineList.length) && !gameOver;  // Değerlendirme - 14
     }
 
     // Method to start the game
     public void run(int coordX, int coordY) {
-        givenCoord = coordX + "," + coordY;
+
         givenRow = coordX;
         givenCol = coordY;
+        givenCoord = coordX + "," + coordY;
 
-        incrementOpenedCellsCount();   // The counter is incremented when the cell is opened
-
+        if (checkCoordInInputList()) {
+            System.out.println("USED COORDS, PLS SELECT A NEW ONE!");
+            return;
+        }
         // Check if the game is won
         if (checkGameWon()) {
-            System.out.println("TEBRİKLER! OYUNU KAZANDINIZ!");
+            System.out.println("CONGRATULATIONS! YOU WON!"); // Değerlendirme - 15
             gameOver = true;
         } else {
             // If the game is not won, the game continues
             draw();
+            incrementOpenedCellsCount();   // The counter is incremented when the cell is opened
         }
     }
 }
-
-
